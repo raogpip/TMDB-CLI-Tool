3ac2d5c868f648268e82320b7cb9fd28be754def
raogpip <vitalik.fedorenko.2018@gmail.com>
2025-02-09 13:35:46 +0100

    nlohmann & curl connection established and tested

--- TMDB CLI Tool.cpp
+++ TMDB CLI Tool.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <curl/curl.h>
+
 
 using json = nlohmann::json;
 
 int main()
 {
-    std::cout << "I am using json now!\n";
+	CURL* curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
+		CURLcode result = curl_easy_perform(curl);
+		if (result != CURLE_OK)
+			std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(result) << '\n';
+		curl_easy_cleanup(curl);
+	}
 }
 
